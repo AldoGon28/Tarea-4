32bf73aac73167ea4568441cd6e58be243f8b707
AldoGon28 <3018632290101@ingenieria.usac.edu.gt>
2022-02-17 21:46:06 -0600

    Primer commit
    Ingreso del formulario del programa

---
+++ Module1.vb
@@ -0,0 +1,41 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim i, j, n As Integer
+        Dim esperar As Char
+
+        While esperar <> "z" And esperar <> "z"
+
+            Try
+
+                j = 1
+                Console.Clear()
+                Console.Write("Ingresar un número entero positivo para calcular el factorial ")
+                n = Console.ReadLine()
+                For i = 1 To n Step 1
+                    j = j * i
+
+                Next
+                Console.Write("El factorial del número ingresado es ")
+                Console.WriteLine(j)
+
+
+
+
+
+
+            Catch ex As Exception
+                Console.WriteLine(ex.Message)
+
+
+            End Try
+
+            Console.Write("Presione la letra z para salir del programa")
+            esperar = Console.ReadKey.KeyChar
+
+
+        End While
+
+    End Sub
+
+End Module
